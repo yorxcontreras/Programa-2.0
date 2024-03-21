9c988ff7aaceae46d180c5b5aa854c1b4df43e1d
Auto Logon <autologon@CSED.LAB>
2024-03-21 08:57:06 -0300

    vercion 2.0

---
+++ Programa.java
@@ -0,0 +1,31 @@
+public class Programa {
+
+    public static void main(String[] args){
+
+  
+        Ejer1.executar();
+        // Ejer2.executar();
+        // Ejer3.executar();
+        // Ejer4.executar();
+        // Ejer5.executar();
+        // Ejer6.executar();
+        // Ejer7.executar();
+        // Ejer8.executar();
+        // Ejer9.executar();
+        // Ejer10.executar();
+        // Ejer11.executar();
+        // Ejer12.executar();
+        // Ejer13.executar();
+        // Ejer14.executar();
+        // Ejer15.executar();
+
+    }
+
+
+    public static void imprimirTitulo(String titulo){
+        System.out.println();
+        System.out.println("------------ " + titulo + " -------------");
+        System.out.println();
+    }
+
+}
